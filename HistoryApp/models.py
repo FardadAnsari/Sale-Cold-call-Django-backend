91107700287dd2918e44766de42eaaf81d8cf58e
saeed <saeedjeddidev@gmail.com>
2025-07-03 19:36:16 +0330

    add account user model

--- HistoryApp/models.py
+++ HistoryApp/models.py
@@ -1,20 +1,15 @@
 from django.db import models
-
-
-
+from accounts_user.models import SaleUser
 
 class HistoryModel(models.Model):
     date = models.DateTimeField()
     sale_session_id = models.IntegerField()
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(SaleUser, on_delete=models.CASCADE, db_column='user_id')
     call_time = models.CharField(max_length=128)
     description = models.TextField()
-
 
     class Meta:
         db_table = 'history'
 
-
     def __str__(self):
         return self.call_time
-
---
+++ accounts_user/middleware.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+
+class JsonResponse404Middleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        if response.status_code == 404:
+            return JsonResponse({
+                'NotFound': 404,
+                'message': 'The requested URL was not found on the server.'
+
+            }, status=404)
+        return response
